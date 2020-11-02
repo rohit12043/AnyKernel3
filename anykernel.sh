feb50462a521147c3bcb22d6eaff93fd4a840dab
rohit12043 <rohitsinha12043@gmail.com>
2020-11-02 23:50:26 +0530

    rolex: change kernel string

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Blizzard kernel for rolex
+kernel.string=Dorime kernel for rolex
 do.devicecheck=1
 do.modules=0
 do.systemless=1
